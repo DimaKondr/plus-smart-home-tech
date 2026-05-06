0eb2771ea506e20a6d5b7d424e192f8457a16fca
Dima Kondratyev <timon.4eb@yandex.ru>
2026-05-06 21:11:50 +0300

    Совершен переход на KafkaTempalte в качестве producer. Доработана видимость конструкторов абстрактных классов.

--- telemetry/collector/src/main/java/ru/practicum/sht/service/HubEventServiceImpl.java
+++ telemetry/collector/src/main/java/ru/practicum/sht/service/HubEventServiceImpl.java
@@ -3,8 +3,6 @@ package ru.practicum.sht.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.avro.specific.SpecificRecordBase;
-import org.apache.kafka.clients.producer.Producer;
-import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import ru.practicum.sht.broker.CollectorTopics;
@@ -16,7 +14,6 @@ import ru.yandex.practicum.kafka.telemetry.event.HubEventAvro;
 @RequiredArgsConstructor
 @Slf4j
 public class HubEventServiceImpl implements HubEventService {
-    //private final Producer<String, SpecificRecordBase> producer;
     private final KafkaTemplate<String, SpecificRecordBase> kafkaTemplate;
     private final HubEventMapper mapper;
 
@@ -25,11 +22,8 @@ public class HubEventServiceImpl implements HubEventService {
         HubEventAvro avroData = mapper.toAvro(event);
         String topic = CollectorTopics.TELEMETRY_HUBS_V1;
         String key = avroData.getHubId();
-
-        //ProducerRecord<String, SpecificRecordBase> record = new ProducerRecord<>(topic, key, avroData);
         log.info("Готовы данные в формате Avro: >>> {} <<< для отправки в Kafka-топик: >>> {} <<<", avroData, topic);
 
-        //producer.send(record);
         kafkaTemplate.send(topic, key,avroData);
     }
 
